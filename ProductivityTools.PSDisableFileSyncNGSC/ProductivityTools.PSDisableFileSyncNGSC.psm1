db1c8c259b616083cc3ce6880ad5463937903b20
pwujczyk <pwujczyk@ecovadis.com>
2020-02-27 17:15:58 +0100

    Automatic Commit

--- ProductivityTools.PSDisableFileSyncNGSC/ProductivityTools.PSDisableFileSyncNGSC.psm1
+++ ProductivityTools.PSDisableFileSyncNGSC/ProductivityTools.PSDisableFileSyncNGSC.psm1
@@ -1,15 +1,19 @@
 function Reset-DisableFileSyncNGSC {
 
+	[cmdletbinding()]
+	param ()	
+	
 	$registryPath = "HKLM:\Software\Policies\Microsoft\Windows\OneDrive"
 	$Name = "DisableFileSyncNGSC"
 	$value = "0"
 	if(!(Test-Path $registryPath))
 	{
+		Write-Verbose "Path not exists, creating"
 		New-Item -Path $registryPath -Force | Out-Null
-		New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force
 	}
-	else
-	{
-		New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force	
-	}
+
+	Write-Verbose "Setting 0 Value to $registryPath $name"
+	$out=New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force	
+	Write-Verbose $out;
+	
 }
